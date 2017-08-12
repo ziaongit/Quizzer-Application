4e79fae79f8ed4f28ed8d5000c44a20b0e007b8b
Zia <zia_gt@yahoo.com>
2017-08-12 20:59:20 +0200

    Mockup done

---
+++ resources/js/main.js
@@ -0,0 +1,42 @@
+$(function(){
+
+    (function startQuiz(){
+        
+        this.loadQuiz = function(){
+            $('.panel_one h1').show('drop', 500, function(){
+                $('.start_quiz').addClass('started', 500);
+            });
+
+            $('.start_quiz').on('click', function(){
+                showPanel(1);
+            });
+        };
+
+        // ShowPanel 
+        this.showPanel = function(position){
+            var current = $('div[data-panel="'+ (position - 1) +'"]');
+            current.find('.wrapper').animate({left: "-=100px", opacity:0},500, function(){
+                
+                current.addClass('hidden');
+
+                var next = $('div[data-panel="'+ position +'"]');
+                next.removeClass('hidden');
+                showNext(next);
+
+            });
+        };
+
+        this.showNext = function(next){
+            var wrapper = next.find('wrapper');
+            wrapper.fadeIn('500', function(){
+
+            });
+        };
+
+
+
+
+        loadQuiz();
+
+    })();
+});
